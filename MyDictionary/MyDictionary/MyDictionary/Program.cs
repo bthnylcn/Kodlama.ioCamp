425c01a970629df0c1bc7f3cd68aeebd660c3147
Batuhan Yalcin <batuhannyalcinn@gmail.com>
2021-02-22 23:26:16 +0300

    #4

---
+++ MyDictionary/MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> cityList = new MyDictionary<int, string>();
+
+            cityList.Add(33, "Mersin");
+            cityList.Add(34, "İstanbul");
+            cityList.Add(35, "İzmir");
+            cityList.Add(06, "Ankara");
+            cityList.Add(01, "Adana");
+        }
+    }
+}
