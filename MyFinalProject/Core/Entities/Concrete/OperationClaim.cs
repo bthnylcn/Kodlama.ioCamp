78940aab5d48687d71c9d09231bb462fe705036d
bthnylcn <batuhannyalcinn@gmail.com>
2021-03-18 16:56:18 +0300

    #14

--- MyFinalProject/Core/Entities/Concrete/OperationClaim.cs
+++ MyFinalProject/Core/Entities/Concrete/OperationClaim.cs
@@ -1,6 +1,6 @@
 ﻿namespace Core.Entities.Concrete
 {
-    public class OperationClaims:IEntity
+    public class OperationClaim:IEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
